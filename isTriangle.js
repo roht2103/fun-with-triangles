dde6ba901319d8b18253dbfcb17e0f52fddf41bf
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-06-22 21:00:00 +0530

    validate data

--- isTriangle.js
+++ isTriangle.js
@@ -1,4 +1,18 @@
-let angle1=document.getElementById('angle1').value;
-let angle2=document.getElementById('angle2').value;
-let angle3=document.getElementById('angle3').value;
-const submitBtn=document.getElementById('submitBtn');
+let angle1 = document.getElementById("angle1");
+let angle2 = document.getElementById("angle2");
+let angle3 = document.getElementById("angle3");
+const submitBtn = document.getElementById("submitBtn");
+const form = document.querySelector("form");
+form.addEventListener("submit", formHandler);
+function formHandler(e) {
+  e.preventDefault();
+  validateData(angle1.value,angle2.value,angle3.value);
+}
+
+function validateData(a1, a2, a3) {
+  if (a1 && a2 && a3) {
+    console.log(a1 + " "+ a2 +" "+ a3)
+  } else {
+    console.log("error")
+  }
+}
