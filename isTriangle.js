00194e2b052624118a1ededc17b0896ba944b2de
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-06-22 20:11:02 +0530

    retrieve HTML element values

---
+++ isTriangle.js
@@ -0,0 +1,4 @@
+let angle1=document.getElementById('angle1').value;
+let angle2=document.getElementById('angle2').value;
+let angle3=document.getElementById('angle3').value;
+const submitBtn=document.getElementById('submitBtn');
